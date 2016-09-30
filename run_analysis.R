b9ab7e6f028a66a060325d77e4e35d8ca4e17e1f
RageMaxBY <ragemaxe@yandex.ru>
2016-09-30 18:04:12 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,13 @@
-library(dplyr)
+library(dplyr)  
+
+filename <- "getdata_projectfiles_UCI HAR Dataset.zip"  
+
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+} 
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
